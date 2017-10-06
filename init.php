af88459437f586c49097684954104237964db70a
Daniel <lanigela@gmail.com>
2017-10-06 11:50:38 -0400

    using filter

--- init.php
+++ init.php
@@ -26,7 +26,8 @@ $active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' 
 if ( in_array( 'woocommerce/woocommerce.php', $active_plugins) ) {
   // Put your plugin code here
   // add player after single_excerpt(short description) and single_meta
-  add_action( 'woocommerce_before_single_product_summary', 'woocommerce_template_single_clara', 10 );
+  //add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_clara', 25 );
+  add_filter( 'woocommerce_single_product_image_thumbnail_html', 'woocommerce_template_single_clara_filter' );
 }
 
 if ( ! function_exists(woocommerce_template_single_clara) ) {
@@ -34,6 +35,21 @@ if ( ! function_exists(woocommerce_template_single_clara) ) {
     global $product;
     load_template(rtrim(plugin_dir_path(__FILE__),'/') . '/templates/single-product/clara-player.php');
 
+    wp_enqueue_script( 'claraPlayer', 'https://clara.io/js/claraplayer.min.js');
+    // load scripts to init clara player
+    wp_enqueue_script( 'claraPlayerControl', rtrim(plugin_dir_url(__FILE__),'/') . '/assets/js/ClaraForPaletteenvy.js');
+    $dataToBePassed = array(
+      'name' => $product->get_name()
+    );
+    // variables will be json encoded here
+    wp_localize_script('claraPlayerControl', 'php_vars', $dataToBePassed);
+  }
+}
+
+if ( ! function_exists(woocommerce_template_single_clara_filter ) ) {
+  function woocommerce_template_single_clara_filter($html, $id) {
+    $html = "<div id='clara-embed'></div>" . $html;
+    global $product;
     wp_enqueue_script( 'claraPlayer', 'https://clara.io/js/claraplayer.min.js');
     // load scripts to init clara player
     wp_enqueue_script( 'claraPlayerControl', rtrim(plugin_dir_url(__FILE__),'/') . '/assets/js/ClaraForPaletteenvy.js');
